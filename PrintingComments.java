5322db0a74898e430a700c4d519c1a4e989d94cb
Eric Fabroa <eric.fabroa@ycdsbk12.ca>
2024-02-07 01:23:34 -0500

    Create PrintingComments.java

---
+++ PrintingComments.java
@@ -0,0 +1,21 @@
+class PrintingComments extends ConsoleProgram {
+  
+  public void run() {
+
+    // Say hello
+    System.out.println("Hi");
+    System.out.println("There.");
+
+    // Say hello on the same line
+    System.out.print("Hi ");
+    System.out.print("There.");
+
+    /*
+    this paragraph
+    will be ignored
+    It is a multiline comment
+    */
+    
+
+  }
+}
